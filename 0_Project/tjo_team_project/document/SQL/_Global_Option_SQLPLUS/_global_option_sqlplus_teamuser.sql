caeeea3eefb19ceaacd723bc217e7db862805b5c
NightOwl <steelman2019e@gmail.com>
2020-01-22 20:44:01 +0900

    Update _global_option_sqlplus_teamuser.sql
    user_info 테이블 - sqlplus 조회 글로벌 옵션 추가.

--- 0_Project/tjo_team_project/document/SQL/_Global_Option_SQLPLUS/_global_option_sqlplus_teamuser.sql
+++ 0_Project/tjo_team_project/document/SQL/_Global_Option_SQLPLUS/_global_option_sqlplus_teamuser.sql
@@ -5,4 +5,15 @@ col notice_title format a30;
 col notice_content format a30;
 col notice_author format a30;
 
+col user_id format a30;
+col user_pwd format a30;
+col user_name format a30;
+col user_nickname format a30;
+col user_tel format a30;
+col user_email format a30;
+col user_addr format a30;
+col user_ip format a30;
+col user_grade format a30;
+
+
 --  TODO 글로벌 옵션 마저 작성할 것, --
