f0b8a7bc7e62024a9a002784fa1442c7d0270849
NightOwl <steelman2019e@gmail.com>
2020-02-03 19:49:25 +0900

    +관리자 상영정보 영역 테이블 생성 SQL
    { 테이블 생성 및 트리거, Sample data } 기록 SQL파일

---
+++ 0_Project/tjo_team_project/document/SQL/14_screen_schedule.sql
@@ -0,0 +1,32 @@
+﻿-- 0. 상영영화 등록정보 시퀀스 생성.
+create sequence seq_screen_schedule_idx;
+
+-- 1. 상영영화 등록정보 테이블 생성.
+create table screen_schedule
+(
+	screen_schedule_idx int,
+	screen_regdata_idx int,
+	screen_idx int,
+	screen_schedule_date VARCHAR(255) not null,
+	screen_schedule_time VARCHAR(255) not null
+);
+
+-- 2. 제약조건 추가.
+alter table screen_schedule add constraint pk_screen_schedule_idx primary key(screen_schedule_idx);
+alter table screen_schedule add constraint fk_screen_schedule_screen_idx foreign key(screen_idx) references screen(screen_idx) on delete cascade;
+
+-- 3. sample data 삽입.
+--p_key / 강남 1상영관 / 영화 국제등록코드 / 겨울왕국2.
+insert into screen_regdata values(seq_screen_regdata_idx.nextVal, 1, 'F48331', '겨울왕국2' );
+--p_key / 신촌 1상영관 / 영화 국제등록코드 / 겨울왕국의 무민.
+insert into screen_regdata values(seq_screen_regdata_idx.nextVal, 2, 'F41181', '겨울왕국의 무민' );
+--p_key / 홍대 1상영관 / 영화 국제등록코드 / 겨울왕국.
+insert into screen_regdata values(seq_screen_regdata_idx.nextVal, 3, 'F32196', '겨울왕국' );
+
+--p_key / 강남 2상영관 / 영화 국제등록코드 / 겨울왕국2.
+insert into screen_regdata values(seq_screen_regdata_idx.nextVal, 4, 'F41181', '겨울왕국의 무민' );
+--p_key / 강남 3상영관 / 영화 국제등록코드/ 겨울왕국
+insert into screen_regdata values(seq_screen_regdata_idx.nextVal, 5, 'F32196', '겨울왕국' );
+
+-- 4. 적용.
+commit
--- 0_Project/tjo_team_project/document/SQL/_Global_Option_SQLPLUS/_global_option_sqlplus_teamuser.sql
+++ 0_Project/tjo_team_project/document/SQL/_Global_Option_SQLPLUS/_global_option_sqlplus_teamuser.sql
@@ -15,16 +15,4 @@ col user_addr format a30;
 col user_ip format a30;
 col user_grade format a30;
 
-col tht_name format a30;
-col tht_tel format a30;
-col tht_area format a30;
-col tht_location format a30;
-
-col DOCID format a30;
-col scr_reg_title format a30;
-
-col scr_d_date format a30;
-col scr_d_time format a30;
-col DOCID format a30;
-
 --  TODO 글로벌 옵션 마저 작성할 것, --
