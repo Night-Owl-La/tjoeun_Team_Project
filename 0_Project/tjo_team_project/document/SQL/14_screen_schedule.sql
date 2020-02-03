19e14d4aedcfeff63933ea35471c46513847f617
NightOwl <steelman2019e@gmail.com>
2020-02-03 19:56:28 +0900

    Update 14_screen_schedule.sql

--- 0_Project/tjo_team_project/document/SQL/14_screen_schedule.sql
+++ 0_Project/tjo_team_project/document/SQL/14_screen_schedule.sql
@@ -6,27 +6,25 @@ create table screen_schedule
 (
 	screen_schedule_idx int,
 	screen_regdata_idx int,
-	screen_idx int,
 	screen_schedule_date VARCHAR(255) not null,
 	screen_schedule_time VARCHAR(255) not null
 );
 
 -- 2. 제약조건 추가.
 alter table screen_schedule add constraint pk_screen_schedule_idx primary key(screen_schedule_idx);
-alter table screen_schedule add constraint fk_screen_schedule_screen_idx foreign key(screen_idx) references screen(screen_idx) on delete cascade;
+alter table screen_schedule add constraint fk_screen_schedule_regdata_idx foreign key(screen_regdata_idx) references screen_regdata(screen_regdata_idx) on delete cascade;
 
 -- 3. sample data 삽입.
---p_key / 강남 1상영관 / 영화 국제등록코드 / 겨울왕국2.
-insert into screen_regdata values(seq_screen_regdata_idx.nextVal, 1, 'F48331', '겨울왕국2' );
---p_key / 신촌 1상영관 / 영화 국제등록코드 / 겨울왕국의 무민.
-insert into screen_regdata values(seq_screen_regdata_idx.nextVal, 2, 'F41181', '겨울왕국의 무민' );
---p_key / 홍대 1상영관 / 영화 국제등록코드 / 겨울왕국.
-insert into screen_regdata values(seq_screen_regdata_idx.nextVal, 3, 'F32196', '겨울왕국' );
-
---p_key / 강남 2상영관 / 영화 국제등록코드 / 겨울왕국2.
-insert into screen_regdata values(seq_screen_regdata_idx.nextVal, 4, 'F41181', '겨울왕국의 무민' );
---p_key / 강남 3상영관 / 영화 국제등록코드/ 겨울왕국
-insert into screen_regdata values(seq_screen_regdata_idx.nextVal, 5, 'F32196', '겨울왕국' );
+--p_key / 상영등록 일련번호 / 2020-02-03 / 20:00.
+insert into screen_schedule values(seq_screen_schedule_idx.nextVal, 1, '2020-02-03', '20:00' );
+--p_key / 상영등록 일련번호 / 2020-02-03 / 22:00.
+insert into screen_schedule values(seq_screen_schedule_idx.nextVal, 2, '2020-02-03', '22:00' );
+--p_key / 상영등록 일련번호 / 2020-02-04 / 20:00.
+insert into screen_schedule values(seq_screen_schedule_idx.nextVal, 3, '2020-02-04', '20:00' );
+--p_key / 상영등록 일련번호 / 2020-02-03 / 09:00.
+insert into screen_schedule values(seq_screen_schedule_idx.nextVal, 4, '2020-02-03', '09:00' );
+--p_key / 상영등록 일련번호 / 2020-02-03 / 03:00.
+insert into screen_schedule values(seq_screen_schedule_idx.nextVal, 5, '2020-02-03', '03:00' );
 
 -- 4. 적용.
 commit
