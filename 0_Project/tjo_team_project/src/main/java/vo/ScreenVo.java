fd6f2e9fc5b6a94360541f46dc40d9327067a3e2
NightOwl <steelman2019e@gmail.com>
2020-02-09 05:42:35 +0900

    Create ScreenVo.java
    상영관 VO

---
+++ 0_Project/tjo_team_project/src/main/java/vo/ScreenVo.java
@@ -0,0 +1,40 @@
+package vo;
+
+public class ScreenVo {
+	int screen_idx;
+	int theater_idx;
+	int screen_id;
+	int screen_classification;
+
+	public int getScreen_idx() {
+		return screen_idx;
+	}
+
+	public void setScreen_idx(int screen_idx) {
+		this.screen_idx = screen_idx;
+	}
+
+	public int getTheater_idx() {
+		return theater_idx;
+	}
+
+	public void setTheater_idx(int theater_idx) {
+		this.theater_idx = theater_idx;
+	}
+
+	public int getScreen_id() {
+		return screen_id;
+	}
+
+	public void setScreen_id(int screen_id) {
+		this.screen_id = screen_id;
+	}
+
+	public int getScreen_classification() {
+		return screen_classification;
+	}
+
+	public void setScreen_classification(int screen_classification) {
+		this.screen_classification = screen_classification;
+	}
+}
