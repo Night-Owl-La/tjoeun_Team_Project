529d3f8b62fd8c8fd3d6bc78a1e62a5ad7d67bad
NightOwl <steelman2019e@gmail.com>
2020-02-09 05:36:33 +0900

    Update Query.sql
    1. 확장자 변경,
    2. 상영 등록된 영화에 대한 모든 정보 조회 쿼리 추가.

--- 0_Project/tjo_team_project/document/SQL/_Query/Query.sql
+++ 0_Project/tjo_team_project/document/SQL/_Query/Query.sql
@@ -5,8 +5,7 @@ select
  theater.theater_area 영화관지역,
  theater.theater_location 영화관위치,
  screen.screen_id 상영관번호, 
- screen.screen_classification 상영관구분, 
- screen.screen_seat_totalcount 상영관좌석_총개수
+ screen.screen_classification 상영관구분
 from 
  theater inner join screen on theater.theater_idx = screen.theater_idx
  inner join screen_regdata on screen_regdata.screen_idx = screen.screen_idx
@@ -20,8 +19,7 @@ select
  theater.theater_area 영화관지역,
  theater.theater_location 영화관위치,
  screen.screen_id 상영관번호, 
- screen.screen_classification 상영관구분, 
- screen.screen_seat_totalcount 상영관좌석_총개수
+ screen.screen_classification 상영관구분 
 from 
  screen_regdata inner join screen on screen_regdata.screen_idx = screen.screen_idx
  inner join theater on theater.theater_idx = screen.theater_idx
@@ -35,7 +33,6 @@ select
  theater.theater_location 영화관위치,
  screen.screen_id 상영관번호, 
  screen.screen_classification 상영관구분,
- screen.screen_seat_totalcount 상영관좌석_총개수,
  screen_schedule.screen_schedule_date 상영날짜,
  screen_schedule.screen_schedule_time 상영시간
 from 
@@ -50,7 +47,6 @@ select
  theater.theater_name 영화관이름,
  screen.screen_id 상영관번호, 
  screen.screen_classification 상영관구분,
- screen.screen_seat_totalcount 상영관좌석_총개수,
  screen_schedule.screen_schedule_date 상영날짜,
  screen_schedule.screen_schedule_time 상영시간,
  screen_seat.screen_seat_id 좌석번호,
@@ -61,3 +57,24 @@ from
  inner join screen_schedule on screen_schedule.screen_regdata_idx = screen_regdata.screen_regdata_idx
  inner join screen_seat on screen.screen_idx = screen_seat.screen_idx
 where theater.theater_name = '무비에어리어 강남점' and screen_regdata.screen_regdata_movietitle = '겨울왕국2' and screen_schedule.screen_schedule_date = '2020-02-03'
+
+-- 상영 등록된 모든 영화에 대한 모든 정보.
+select 
+    sr.docid 영화_국제등록코드,
+    sr.screen_regdata_movietitle 영화이름, 
+    t.theater_name 영화관_이름,
+    t.theater_tel 영화관_연락처,
+    t.theater_area 영화관_지역,
+    t.theater_location 영화관_위치,
+    s.screen_id 상영관_번호,
+    s.screen_classification 상영관_구분,
+    ss.screen_seat_total_amount 상영관_좌석_총개수
+from 
+    screen s inner join theater t on s.theater_idx = t.theater_idx
+    inner join screen_regdata sr on sr.screen_idx = s.screen_idx
+    inner join 
+        (select screen.screen_idx, count(*) screen_seat_total_amount 
+        from screen_seat inner join screen on screen_seat.screen_idx = screen.screen_idx
+        GROUP BY screen.screen_idx) ss 
+    on ss.screen_idx = s.screen_idx
+order by sr.docid;
