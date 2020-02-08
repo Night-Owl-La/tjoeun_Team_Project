909cba7e15802c5aabba90cb048680f3b2ab1e27
NightOwl <steelman2019e@gmail.com>
2020-02-09 05:42:15 +0900

    Create Screen_RegdataVo.java
    상영정보를 담는 VO

---
+++ 0_Project/tjo_team_project/src/main/java/vo/Screen_RegdataVo.java
@@ -0,0 +1,40 @@
+package vo;
+
+public class Screen_RegdataVo {
+	int screen_regdata_idx;
+	int screen_idx;
+	String DOCID;
+	String screen_regdata_movietitle;
+
+	public int getScreen_regdata_idx() {
+		return screen_regdata_idx;
+	}
+
+	public void setScreen_regdata_idx(int screen_regdata_idx) {
+		this.screen_regdata_idx = screen_regdata_idx;
+	}
+
+	public int getScreen_idx() {
+		return screen_idx;
+	}
+
+	public void setScreen_idx(int screen_idx) {
+		this.screen_idx = screen_idx;
+	}
+
+	public String getDOCID() {
+		return DOCID;
+	}
+
+	public void setDOCID(String dOCID) {
+		DOCID = dOCID;
+	}
+
+	public String getScreen_regdata_movietitle() {
+		return screen_regdata_movietitle;
+	}
+
+	public void setScreen_regdata_movietitle(String screen_regdata_movietitle) {
+		this.screen_regdata_movietitle = screen_regdata_movietitle;
+	}
+}
