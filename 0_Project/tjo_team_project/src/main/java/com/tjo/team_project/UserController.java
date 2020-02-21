f5bd6d8a1ab5e8e6180b2edb08cf3e3408ffc6fa
seungchulsong <58935840+seungchulsong@users.noreply.github.com>
2020-02-21 19:16:06 +0900

    Update UserController.java
    1. 카카오 로그인 추가.
    2. 로그아웃시 url 이 존재하면 해당 url로 돌아감.

--- 0_Project/tjo_team_project/src/main/java/com/tjo/team_project/UserController.java
+++ 0_Project/tjo_team_project/src/main/java/com/tjo/team_project/UserController.java
@@ -9,9 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import dao.KakaoUserDao;
 import dao.UserDao;
+import vo.KakaoUserVo;
 import vo.UserVo;
 
 @Controller
@@ -27,19 +30,76 @@ public class UserController {
 	@Autowired
 	UserDao user_dao;
 	
+	@Autowired
+	KakaoUserDao kakaouser_dao;
+	
+	
 
+	public void setUser_dao(UserDao user_dao) {
+		this.user_dao = user_dao;
+	}
+
+	public void setKakaouser_dao(KakaoUserDao kakaouser_dao) {
+		this.kakaouser_dao = kakaouser_dao;
+	}
+
+	
+	
+	
+	// 카카오 로그인
+	@RequestMapping( value = "/user/kakao_login.do", method = RequestMethod.GET)
+	@ResponseBody
+	public String kakao_login(KakaoUserVo vo, Model model) {
+		
+		//System.out.println(vo.getKakao_user_email());
+		
+		// 1. kakaouser_dao.selectOne(email) // List<KakaoUserVo>
+		
+		KakaoUserVo kakao_user = kakaouser_dao.selectOne(vo.getKakao_user_email());
+		
+	
+		if(kakao_user!=null){ // 이미 있을 때
+			session.setAttribute("kakao_user", kakao_user);
+			// session -> kakaouser
+		} 
+		else{ // 없을 때
+			model.addAttribute("reason", "use_id");
+			// return "reason", "id 가 없음"
+		}
+		
+		return "redirect:list.do";
+		  
+	}
+	
+     	/*
+	  	if(session == null){
+	  		response.sendRedirect(requset.getContextPath()+"/user/login_form.jsp);
+	  	}
+	    */
+		
+			
+		// 3. 있으면-> 이미 있으니까 패스->(insert) -> login 성공
+		// 3-1 없으면 -> 가입할꺼냐고 물어본다. 
+		// 3-2 yes-> insert(vo) -> login 처리.
+		// 3-2.1 -> no -> 로그인 실패.
+		
+		//kakaouser_dao.insert(vo);
+
+	
+	
+	
 	@RequestMapping("/user/login_form.do")
 	public String login_form() {
 		
 		return "user/user_login_form";
 	}
 	
+
 	
 	@RequestMapping("/user/login.do")
 	public String login(String user_id, String user_pwd, String url,Model model) {
-		
+	
 	UserVo user = user_dao.selectOne(user_id);
-		
 	
 	if(user==null) {
 		model.addAttribute("reason", "fail_id");
@@ -59,16 +119,21 @@ public class UserController {
 	if(!url.isEmpty())
 		return "redirect:" + url;
 		
-	return "redirect:../board/list.do";
-	}
+	return "redirect:../user/list.do";
 	
+	}
+		
 	
 	@RequestMapping("/user/logout.do")
-	public String logout() {
+	public String logout(String url) {
 		
-		session.removeAttribute("user");
-		
-		return "redirect:../board/list.do";
+	session.removeAttribute("user");
+	
+	if(!url.isEmpty()) 
+		return "redirect:" + url;
+	
+	
+		return "redirect:../user/list.do";
 	}
 	
 	
