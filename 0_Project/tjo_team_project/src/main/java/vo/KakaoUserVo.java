48a1783df5f27f194579084b667f6db204f476f2
seungchulsong <58935840+seungchulsong@users.noreply.github.com>
2020-02-21 19:20:17 +0900

    Create KakaoUserVo.java
    1. db에서 얻어온  데이터를 매핑하는 vo.

---
+++ 0_Project/tjo_team_project/src/main/java/vo/KakaoUserVo.java
@@ -0,0 +1,25 @@
+package vo;
+
+public class KakaoUserVo {
+
+	int kakao_idx;
+	String kakao_user_email;
+	
+	
+	public String getKakao_user_email() {
+		return kakao_user_email;
+	}
+	public void setKakao_user_email(String kakao_user_email) {
+		this.kakao_user_email = kakao_user_email;
+	}
+	public int getKakao_idx() {
+		return kakao_idx;
+	}
+	public void setKakao_idx(int kakao_idx) {
+		this.kakao_idx = kakao_idx;
+	}
+	
+	
+	
+	
+}
